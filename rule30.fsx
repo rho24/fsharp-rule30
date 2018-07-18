d55b9f9ed2b1db95b55993476289d54d1f632ad7
Richard Oliver <oliverr@parliament.uk>
2018-07-18 16:43:26 +0100

    Init

---
+++ rule30.fsx
@@ -0,0 +1,39 @@
+let halfWidth = 50
+open System.Text
+let firstRow = Array.concat [Array.zeroCreate<int> halfWidth; [|1|]; Array.zeroCreate halfWidth]
+
+let rule30 = function
+    | [|1;0;0|]
+    | [|0;1;1|]
+    | [|0;1;0|]
+    | [|0;0;1|] -> 1
+    | _ -> 0
+
+let printRow row = 
+    row
+    |> Array.map (fun e -> if e = 1 then 'X' else ' ')
+    |> System.String
+
+printRow firstRow
+
+let generateRow (prevRow: int[]) = 
+    Array.concat [
+        [|0|];
+        prevRow
+            |> Seq.windowed 3
+            |> Seq.map rule30
+            |> Seq.toArray
+        [|0|];
+    ]
+
+firstRow |> generateRow |> printRow
+
+let rows = 
+    [|0..halfWidth|]
+    |> Array.scan (fun row _ -> generateRow row) firstRow
+
+let output = new StringBuilder("\n")
+
+rows
+    |> Array.map (printRow)
+    |> Array.iter (Printf.bprintf output "%s\n")
